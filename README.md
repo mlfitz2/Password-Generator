e4854f0061cdd65d0de4355e36d43f966769b53a
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-03-02 20:59:22 -0800

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Password-Generator
+
+Deployed application: https://mlfitz2.github.io/Password-Generator/
+GitHub Repository: https://github.com/mlfitz2/Password-Generator
+
+This application is a password generator that allows users to define their own parameters for what their password should be like. The user can select its length (anywhere from 8 to 128 characters) and which types of characters should be included from uppercase letters, lowercase letters, numbers and special characters. 
+
+The application was built using HTML for the user interface and JavaScript for the functionality. 
+
+![Screenshot-1](./screenshots/homescreen.JPG)
+![Screenshot-2](./screenshots/password.JPG)
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function writePassword() {
   password = "";
   var passwordText = document.querySelector("#password");
   var passwordLength = 0;
-  var uppercaseLetters = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '-', '/'];
@@ -14,41 +14,41 @@ function writePassword() {
 
   //Select password length
   var chooseLength = window.prompt('How long would you like your password to be? Choose between 8 and 128 characters.');
-  if (chooseLength > 7 && chooseLength < 127) {
+  if (chooseLength > 7 && chooseLength < 129) {
     passwordLength = chooseLength
-    console.log(passwordLength)
   } else {
     window.alert('Invalid response. Please enter a number between 8 and 128.')
+    return
   }
 
   //Select if uppercase letters will be included
   var chooseUppercase = window.confirm('Would you like uppercase letters to be included?');
   if (chooseUppercase) {
-      selectedCharacters = selectedCharacters + uppercaseLetters 
+      selectedCharacters = selectedCharacters.concat(uppercaseLetters)
   }
 
   //Select if lowercase letters will be included
   var chooseLowercase = window.confirm('Would you like lowercase letters to be included?');
   if (chooseLowercase) {
-      selectedCharacters = selectedCharacters + lowercaseLetters 
+      selectedCharacters = selectedCharacters.concat(lowercaseLetters) 
   }
-
+console.log(selectedCharacters)
   //Select if numbers will be included
     var chooseNumbers = window.confirm('Would you like numbers to be included?');
     if (chooseNumbers) {
-        selectedCharacters = selectedCharacters + numbers 
+        selectedCharacters = selectedCharacters.concat(numbers) 
     }
 
   //Select if special characters will be included
   var chooseSpecialCharacters = window.confirm('Would you like special characters to be included?');
   if (chooseSpecialCharacters) {
-      selectedCharacters = selectedCharacters + specialCharacters 
+      selectedCharacters = selectedCharacters.concat(specialCharacters) 
   }
 
+  //Put random characters from the selected categories together, up to the specified number
   for (var i = 0; i < passwordLength; i++) {
     password = password + selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)]
     console.log(password)
-   
   } 
 
 
